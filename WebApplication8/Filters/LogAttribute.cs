c5f1bd8e3093b1ce479834d0d19c92a16c82dd64
ACER <ACER@DESKTOP-1BR1UTS>
2022-01-03 17:21:37 +0530

    Add project files.

---
+++ WebApplication8/Filters/LogAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Filters
+{
+
+    public class LogAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+        public override void OnActionExecuted(ActionExecutedContext filtercontext)
+        {
+            base.OnActionExecuted(filtercontext);
+        }
+        public override void OnResultExecuting(ResultExecutingContext filtercontext)
+        {
+            base.OnResultExecuting(filtercontext);
+        }
+        public override void OnResultExecuted(ResultExecutedContext filtercontext)
+        {
+            base.OnResultExecuted(filtercontext);
+        }
+    }
+}
